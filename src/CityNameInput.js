b3a3af930c75cb0cf15170164d74c3835b3d7a22
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-05-02 17:12:06 -0700

    deleted hard code for city name display

--- src/CityNameInput.js
+++ src/CityNameInput.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function CityNameInput({ handleCityName }) {
   return (
     <section>
-      What is your city name?
+      
       {/* this component should have a prop called setCityName, which is a function */}
       {/* on change, this component should use e.target.value to pass the correct id to the setCityName function */}
       <input onChange={handleCityName} />
